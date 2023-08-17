fe19c1544e0482b5d1ba72186e5908f0f5d0e740
Adrian Ngo <adrian.ngo@ibsolution.de>
2023-08-17 10:03:30 +0200

    Test-wise: cds v7: WorldDataService as a public Service and Maintain World Cities App as a public app: 401 - Unauthorized

--- srv/on-premise.js
+++ srv/on-premise.js
@@ -3,9 +3,9 @@ const cds = require('@sap/cds')
 const log = cds.log('on-premise-service')
 
 module.exports = cds.service.impl(async (srv) => {
-  srv.on('READ', 'Products', async (request) => {
-    const externalProductService = await cds.connect.to('API_PRODUCT_SRV')
-    return externalProductService.tx(request).run(request.query)
-  })
+  // srv.on('READ', 'Products', async (request) => {
+  //   const externalProductService = await cds.connect.to('API_PRODUCT_SRV')
+  //   return externalProductService.tx(request).run(request.query)
+  // })
 })
 
